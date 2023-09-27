4babd92895236071c90f74514a30fbc038c0fc8b
sreeshanath <sreesha.s.nath@gmail.com>
2023-09-27 09:29:46 -0600

    adding solution for the todo list activity

--- resources/js/todo_list.js
+++ resources/js/todo_list.js
@@ -1,43 +1,77 @@
-//create a tasks array. You can use this to persist the list in local storage.
+let tasks = [];
 
-// get the ul element handle. use document.querySelector
+console.log(localStorage.getItem("taskList"));
+
+const list = document.querySelector("ul");
 
 function addItem(){
-    //let's read the task from the HTML and let's check if we read that value correctly.
+    //let's read the task from the HTML
+    let newTask = document.querySelector("#taskItem").value;
+    console.log(newTask);
 
-    //now let's also clear the input textbox so we can accept new tasks
+    document.querySelector("#taskItem").value = "";
+
+    //add it to the task array
+    tasks.push(newTask);
+    console.log(tasks);
 
     //create list item to add to the UI
+    let list_item = document.createElement("li");
 
     //adding bootstrap classes to the li to give it an upgraded look
+    list_item.classList.add("list-group-item");
 
     //create a span for each task row
+    let spanRow = document.createElement("span");
+    spanRow.classList.add("mark-done");
 
-    // creating a checkbox input element. Set type attribute to be "checkbox"
+    // creating a checkbox input element
+    let check = document.createElement("input");
+    check.type="checkbox";
 
-    //adding bootstrap classes to the checkbox to give it an upgraded look. "form-check-input","me-5". element.classList.add()
+    //adding bootstrap classes to the checkbox to give it an upgraded look
+    check.classList.add("form-check-input","me-5");
 
-    //add event listener for click event to checkbox
-    
-    //create a text element for the text of the task. document.createTextNode()
+    //create a text element for the text of the task
+    var taskTextNode = document.createTextNode(newTask);
     
     //append the checkbox and text to the span
+    spanRow.appendChild(check);
+    spanRow.appendChild(taskTextNode);
+    check.addEventListener('click', toggleStatus);
 
-    //create a button to delete the task. set the innerHTML to be "X"
+    //create a button to delete the task
+    var deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "X";
+    deleteBtn.classList.add("btn", "btn-danger", "float-end");
 
-    //adding bootstrap classes to the delete button to give it an upgraded look. "btn", "btn-danger", "float-end"
+    //attach event listener to this button to remove the item from the list
+    deleteBtn.addEventListener('click', deleteTask);
 
-    //attach event listener to the click event of this button to remove the item from the list
 
-    //append all span and delete button to the list item
-    
+    //append all the items to the list item
+    list_item.appendChild(spanRow);
+    list_item.appendChild(deleteBtn);
+
     //append list item to ul
+    list.appendChild(list_item);
 
 }
 
-//changes appearance of task based on completeness. checkbox checked or not. ev.target
+//changes appearance of task based on completeness
+toggleStatus = (ev) => {
+    if(ev.target.checked){
+        ev.target.parentElement.classList.add('done');
+    }
+    else{
+        ev.target.parentElement.classList.remove('done');
+    }
+}
 
-//deletes task from the list. ev.target.parentElement. Get ul element. use removeChild().
+//deletes task from the list
+deleteTask = (ev) =>{
+    document.querySelector("ul").removeChild(ev.target.parentElement);
+}
 
 //fun activity - try to add an edit button to edit tasks that have already been added
 
