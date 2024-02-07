2b800d1302d909b63f552f8784aad5ad7b473973
SreeshaNath <31390313+SreeshaNath@users.noreply.github.com>
2024-02-06 18:14:42 -0700

    Add files via upload

---
+++ todo_list.js
@@ -0,0 +1,40 @@
+
+const list = document.querySelector("ul");
+
+function addItem(){
+    //let's read the task from the HTML
+
+    //add it to the task array
+
+    //create list item to add to the UI
+
+    //adding bootstrap classes to the li to give it an upgraded look
+
+    //create a span for each task row
+
+    // creating a checkbox input element
+
+    //adding bootstrap classes to the checkbox to give it an upgraded look
+
+    //create a text element for the text of the task
+    
+    //append the checkbox and text to the span
+
+    //create a button to delete the task
+
+    //attach event listener to this button to remove the item from the list
+
+
+    //append all the items to the list item
+
+    //append list item to ul
+
+}
+
+//changes appearance of task based on completeness
+
+//deletes task from the list
+
+//fun activity - try to add an edit button to edit tasks that have already been added
+
+
